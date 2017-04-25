6de352a86997a0d67f80cf14d796a91e0dfb59b8
Michael Dunne <dunne@dps008.plants.ox.ac.uk>
2017-04-25 16:00:25 +0100

    Corrected error in cfg_augustus assignment

--- runOrthoFil.sh
+++ runOrthoFil.sh
@@ -2,10 +2,9 @@
 
 bin_hmmer=""		# path to hmmer binaries directory
 bin_augustus=""		# path to augustus binaries directory
-loc_augustus=""		# path to augustus config directory
+cfg_augustus=""		# path to augustus config directory
 scr_augustus=""		# path to augustus scripts directory
 loc_orthofinder="" 	# path to directory containing orthofinder.py
-
 
 export ORTHOFINDER_DIR=$loc_orthofinder
 export AUGUSTUS_CONFIG_PATH=$cfg_augustus
@@ -13,15 +12,17 @@ PATH="$bin_hmmer:$bin_augustus:$scr_augustus:$PATH"
 
 unset R_HOME
 
+
+python OrthoFiller.py \
+	-i    \ #[sequence locations file]
+	-o    \ #[output directory]
+	-c 16
+
+
 python OrthoFiller.py --prep \
 	-g    \ #[orthogroups file]
 	-s    \ #[singletons file]
 	-i    \ #[sequence locations file]
 	-o    \ #[output directory]
 	-c 16
-
-#python OrthoFiller.py \
-#	-i    \ #[sequence locations file]
-#	-o    \ #[output directory]
-#	-c 16
 
